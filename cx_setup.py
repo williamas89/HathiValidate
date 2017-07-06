5d4acbe29a25f27b2ebf610c88f3be617cc68e80
hborcher <hborcher@illinois.edu>
2017-07-06 16:38:29 -0500

    add documentation link

--- cx_setup.py
+++ cx_setup.py
@@ -6,11 +6,11 @@ import pytest
 import hathi_validate
 import platform
 
-# def create_msi_tablename(python_name, fullname):
-#     shortname = python_name[:6].replace("_", "").upper()
-#     longname = fullname
-#     return "{}|{}".format(shortname, longname)
-#
+def create_msi_tablename(python_name, fullname):
+    shortname = python_name[:6].replace("_", "").upper()
+    longname = fullname
+    return "{}|{}".format(shortname, longname)
+
 PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
 MSVC = os.path.join(PYTHON_INSTALL_DIR, 'vcruntime140.dll')
 
@@ -21,35 +21,35 @@ INCLUDE_FILES = [
     #     TODO: BUILD DOCUMENTATION
 ]
 
-# directory_table = [
-#     (
-#         "ProgramMenuFolder",  # Directory
-#         "TARGETDIR",  # Directory_parent
-#         "PMenu|Programs",  # DefaultDir
-#     ),
-#     (
-#         "PMenu",  # Directory
-#         "ProgramMenuFolder",  # Directory_parent
-#         create_msi_tablename(hathi_validate.__title__, hathi_validate.FULL_TITLE)
-#     ),
-# ]
-# shortcut_table = [
-#     (
-#         "startmenuShortcutDoc",  # Shortcut
-#         "PMenu",  # Directory_
-#         "{} Documentation".format(create_msi_tablename(hathi_validate.__title__, hathi_validate.FULL_TITLE)),
-#         "TARGETDIR",  # Component_
-#         "[TARGETDIR]documentation.url",  # Target
-#         None,  # Arguments
-#         None,  # Description
-#         None,  # Hotkey
-#         None,  # Icon
-#         None,  # IconIndex
-#         None,  # ShowCmd
-#         'TARGETDIR'  # WkDir
-#     ),
-#     #TODO: turn back on when documentation exists
-# ]
+directory_table = [
+    (
+        "ProgramMenuFolder",  # Directory
+        "TARGETDIR",  # Directory_parent
+        "PMenu|Programs",  # DefaultDir
+    ),
+    (
+        "PMenu",  # Directory
+        "ProgramMenuFolder",  # Directory_parent
+        create_msi_tablename(hathi_validate.__title__, hathi_validate.FULL_TITLE)
+    ),
+]
+shortcut_table = [
+    (
+        "startmenuShortcutDoc",  # Shortcut
+        "PMenu",  # Directory_
+        "{} Documentation".format(create_msi_tablename(hathi_validate.__title__, hathi_validate.FULL_TITLE)),
+        "TARGETDIR",  # Component_
+        "[TARGETDIR]documentation.url",  # Target
+        None,  # Arguments
+        None,  # Description
+        None,  # Hotkey
+        None,  # Icon
+        None,  # IconIndex
+        None,  # ShowCmd
+        'TARGETDIR'  # WkDir
+    ),
+    #TODO: turn back on when documentation exists
+]
 
 if os.path.exists(MSVC):
     INCLUDE_FILES.append(MSVC)
@@ -73,12 +73,12 @@ setup(
     author_email=hathi_validate.__author_email__,
     options={
         "build_exe": build_exe_options,
-        # "bdist_msi": {
-        #     "data": {
-        #         "Shortcut": shortcut_table,
-        #         "Directory": directory_table
-        #     }
-        # }
+        "bdist_msi": {
+            "data": {
+                "Shortcut": shortcut_table,
+                "Directory": directory_table
+            }
+        }
     },
     executables=[Executable("hathi_validate/cli.py",
                             targetName=target_name, base="Console")],
