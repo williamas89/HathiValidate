d3481b57879d5751690fbe2375f57d3570323410
hborcher <hborcher@illinois.edu>
2017-07-06 15:02:16 -0500

    updated result to have a source

--- hathi_validate/result.py
+++ hathi_validate/result.py
@@ -1,23 +1,44 @@
 import abc
+import typing
+from collections.abc import Collection
 
 
 class Result:
-    message = ""
+
 
     def __init__(self, result_type):
         self.result_type = result_type
+        self.source = None
+        self.message = ""
+
+    def __str__(self) -> str:
+        if self.source:
+            message = '{}: "{}"'.format(self.source, self.message)
+        else:
+            message = '"{}"'.format(self.message)
+        return "{}[{}]{}".format(__class__, self.result_type, message)
 
 
-class ResultSummary:
-    results = []
-    source = None
+class ResultSummary(Collection):
+    def __init__(self):
+        self.results = []
+        self.source = None
 
-    def __add__(self, other):
+    def __iadd__(self, other):
         self.results.append(other)
+        return self
+
+    def __iter__(self) -> typing.Iterator[Result]:
+        return self.results.__iter__()
+
+    def __len__(self):
+        return len(self.results)
+
+    def __contains__(self, x):
+        return x in self.results
 
 
 class AbsResultBuilder(metaclass=abc.ABCMeta):
-
     @abc.abstractmethod
     def create_new_summary(self):
         pass
@@ -36,8 +57,9 @@ class AbsResultBuilder(metaclass=abc.ABCMeta):
 
 
 class ResultSummaryBuilder(AbsResultBuilder):
-    summary = None
-    source = None
+    def __init__(self):
+        self.summary = None
+        self.source = None
 
     def create_new_summary(self):
         self.summary = ResultSummary()
@@ -61,7 +83,8 @@ class SummaryDirector:
     def add_error(self, message):
         new_error = Result("error")
         new_error.message = message
+        new_error.source = self.builder.source
         self.builder.add_result(new_error)
 
-    def construct(self)->ResultSummary:
+    def construct(self) -> ResultSummary:
         return self.builder.get_summary()
