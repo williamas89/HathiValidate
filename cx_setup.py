a5ebd472002de858920b6b1f5e23d29c776c77f7
hborcher <hborcher@illinois.edu>
2017-07-14 16:15:32 -0500

    fixed tests getting compiled to pyc and being impossible to run frozen

--- cx_setup.py
+++ cx_setup.py
@@ -6,20 +6,30 @@ import pytest
 import hathi_validate
 import platform
 
+
 def create_msi_tablename(python_name, fullname):
     shortname = python_name[:6].replace("_", "").upper()
     longname = fullname
     return "{}|{}".format(shortname, longname)
 
+
 PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
 MSVC = os.path.join(PYTHON_INSTALL_DIR, 'vcruntime140.dll')
 
 
+def get_tests():
+    root = "tests"
+    test_files = []
+    for x in filter(lambda x: x.is_file and os.path.splitext(x.name)[1] == ".py", os.scandir(root)):
+        test_files.append(os.path.join(root, x.name))
+    print("Found files {}".format(", ".join(test_files)))
+    return test_files
+
+
 INCLUDE_FILES = [
-    # "tests"
     "documentation.url",
     #     TODO: BUILD DOCUMENTATION
-]
+] + get_tests()
 
 directory_table = [
     (
@@ -53,12 +63,11 @@ shortcut_table = [
 if os.path.exists(MSVC):
     INCLUDE_FILES.append(MSVC)
 
-build_exe_options ={
+build_exe_options = {
     "includes": ["queue", "atexit", "appdirs", 'pkg_resources'] + pytest.freeze_includes(),
     "include_msvcr": True,
-    "packages": ["os", "lxml", "packaging", "six", "appdirs", "tests", "hathi_validate"],
+    "packages": ["os", "lxml", "packaging", "six", "appdirs", "hathi_validate"],
     "excludes": ["tkinter"],
-    "namespace_packages": ["tests"],
     "include_files": INCLUDE_FILES,
 
 }
